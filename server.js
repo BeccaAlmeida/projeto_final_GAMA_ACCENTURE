8b79f73b1f06c9d3a837d2f30aaf21b23bdcf1a1
emerson <emerson.caique@outlook.com>
2020-11-05 19:02:19 -0300

    debug console

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "dist")));
+console.log(path.join(__dirname, "dist", "index.html"));
+console.log(path.join(__dirname, "index.html"));
+
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
