7e153d08c3cefc7ecc84051e8d4a7c9e1e9d3cfe
emerson <emerson.caique@outlook.com>
2020-11-05 19:27:50 -0300

    teste heroku

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "../../dist")));
 
+console.log(path.join(__dirname, "../../dist", "index.html"));
+console.log(path.join(__dirname, "../../dist"));
+
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "../../dist", "index.html"));
 });
