814a24e59af99453fc69b1591a2268333e9ca425
emerson <emerson.caique@outlook.com>
2020-11-05 19:08:25 -0300

    config heroku

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "../../dist")));
 
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "../dist", "index.html"));
+	res.sendFile(path.join(__dirname, "../../dist", "index.html"));
 });
 app.listen(port);
