51674bb0e8ab6cac9e8f4d43b2ad38d7b7a19f14
Deyvesson <deyvessonc@gmail.com>
2020-11-05 23:26:10 -0300

    adicionada a tela de informacoes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./login";
 import Painel from "./painel";
+import Informacoes from "./informacoes"
 import PrivateRoute from "../components/PrivateRoute";
 
 const Pages = () => (
 	<Switch>
 		<Route path="/" component={Login} exact />
 		<PrivateRoute path="/painel" component={Painel} exact />
+		<PrivateRoute path="/informacoes" component={Informacoes} exact />
 	</Switch>
 );
 
