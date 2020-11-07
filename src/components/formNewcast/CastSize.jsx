2b05aaef49402403b998d004ca90a07efaec6eb1
Rebecca Almeida <rebeccaalmeidadesenvolvimento@gmail.com>
2020-11-07 19:07:24 -0300

    Opções de elenco adicionadas

--- src/components/formNewcast/CastSize.jsx
+++ src/components/formNewcast/CastSize.jsx
@@ -4,120 +4,196 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
+import Label from '@material-ui/core/FormLabel';
+import Content from '../base/Content';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
-  },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    minWidth: 180,
   },
 }));
 
-export default function ControlledOpenSelect() {
+export default function GroupedSelect() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [director, setDirector] = React.useState('');
-  const [continuista, setContinuista] = React.useState('');
- /* const [diretorFotografia, setDiretorFotografia] = React.useState('');
-  const [fotografoCena, setFotografoCena] = React.useState('');
-  const [diretorProducao, setDiretorProducao] = React.useState('');
-  const [assitenteCamera, setAssitenteCamera] = React.useState('');
-  const [clapLoader, setClapLoader] = React.useState('');
-  const [figurinista, setFigurinista] = React.useState('');
-  const [guardaRoupeira, setGuardaRoupeira] = React.useState('');
-  const [tecnicoSom, setTecnicoSom] = React.useState('');
-  const [microfonista, setMicrofonista] = React.useState('');
-  const [cenografo, setCenografo] = React.useState('');
-  const [cenotecnico, setCenotecnico] = React.useState('');
-  const [roterista, setRoterista] = React.useState('');
-  const [argumentista, setArgumentista] = React.useState('');
-  const [dialogista, setdialogista] = React.useState('');
-  const [gagsMan, setGagsMan] = React.useState('');
-  const [pesquisador, setPesquisador] = React.useState('');
-  const [desenhoAnimado, setDesenhoAnimado] = React.useState('');
-  const [assistenteDirecao, setAssistenteDirecao] = React.useState('');
-  const [operadorCamera, setOperadorCamera] = React.useState('');
-  const [operadorSteadyCam, setOperadorSteadyCam] = React.useState('');
-  const [assistenteProdutorExec, setAssistenteProdutorExec] = React.useState('');
-  const [assistenteProd, setAssistenteProd] = React.useState('');
-  const [chefeEletricista, setChefeEletricista] = React.useState('');
-  const [eletricista, setEletricista] = React.useState('');
-  const [geradorista, setGeradorista] = React.useState('');
-  const [chefeMaquinista, setChefeMaquinista] = React.useState('');
-  const [maquinista, setMaquinista] = React.useState('');
-  const [operadorVideoAssistent, setoperadorVideoAssistent] = React.useState('');
-  const [contrarregra, setContrregra] = React.useState('');
-  const [maquiador, setMaquiador] = React.useState('');
-  const [editor, setEditor] = React.useState('');
-  const [trilheiro, setTrilheiro] = React.useState('');
-  const [efeitosSpeciais, setEfeitosSpeciais] = React.useState('');
-  const [laboratorista, setLaboratorista] = React.useState('');
-  const [maracdorLuz, setDialogista] = React.useState('');
-  const [montadorNegativo, setMontadorNegativo] = React.useState('');
-  const [open, setOpen] = React.useState(false);*/
-
-  const handleChange = (event) => {
-    setDirector(event.target.value);
-    setContinuista(event.target.value);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
 
   return (
     <div>
-      <Button className={classes.button} onClick={handleOpen}>
-        Monte seu elenco
-      </Button>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-controlled-open-select-label">Diretor</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={director}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={10}>mais de três</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-controlled-open-select-label">continuista</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={continuista}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={10}>mais de três</MenuItem>
-        </Select>
-      </FormControl>
+      <Content>
+        <Label>Monte seu Elenco</Label>
+      </Content>
+      <Content>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Diretor</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Continuísta</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Diretor de Fotográfia</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Fotográfo de Cena</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Diretor de produção</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Assistente de Câmera</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Clap loader</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Figurinista</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Guarda-Roupeira</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Técnico de Som</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Cenógrafo</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Cenotécnico</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Roterista</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Argumentista</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Dialogista</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Gas Man</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-native-select">Pesquisador</InputLabel>
+          <Select native defaultValue="" id="diretor">
+            <option aria-label="None" value="" />
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={10}>4 á 10</option>
+          </Select>
+        </FormControl>
+      </Content>
     </div>
   );
 }
